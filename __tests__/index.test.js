9a39f27fdd295aa15b2ce7ebdf3b175a9480c4a8
swiftie26 <ayesha.rana2604@gmail.com>
2024-09-21 18:29:48 +1000

    Update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,8 @@
 const request = require('supertest');
 const http = require('http');
-const hostname = '127.0.0.1';
+
+// Use '0.0.0.0' to listen on all interfaces
+const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
@@ -12,7 +14,7 @@ const server = http.createServer((req, res) => {
 describe('GET /', () => {
   it('should return Hello, Jenkins Pipeline!', async () => {
     server.listen(port, hostname);
-    const res = await request(`http://${hostname}:${port}`).get('/');
+    const res = await request(`http://127.0.0.1:${port}`).get('/');
     expect(res.statusCode).toBe(200);
     expect(res.text).toBe('Hello, Jenkins Pipeline!\n');
     server.close();
